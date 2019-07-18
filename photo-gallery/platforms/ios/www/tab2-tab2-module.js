7fb8355b99414e1959c0acfa2c6b9b1c9144aa40
lk220203006@126.com <lk220203006@126.com>
2019-07-18 14:39:15 +0800

    add

---
+++ photo-gallery/platforms/ios/www/tab2-tab2-module.js
@@ -0,0 +1,174 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["tab2-tab2-module"],{
+
+/***/ "./src/app/services/photo.service.ts":
+/*!*******************************************!*\
+  !*** ./src/app/services/photo.service.ts ***!
+  \*******************************************/
+/*! exports provided: PhotoService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PhotoService", function() { return PhotoService; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _ionic_native_camera_ngx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @ionic-native/camera/ngx */ "./node_modules/@ionic-native/camera/ngx/index.js");
+/* harmony import */ var _ionic_storage__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @ionic/storage */ "./node_modules/@ionic/storage/fesm5/ionic-storage.js");
+
+
+
+
+var PhotoService = /** @class */ (function () {
+    function PhotoService(camera, storage) {
+        this.camera = camera;
+        this.storage = storage;
+        this.photos = [];
+    }
+    PhotoService.prototype.takePicture = function () {
+        var _this = this;
+        var options = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        };
+        this.camera.getPicture(options).then(function (imageData) {
+            _this.photos.unshift({
+                data: 'data:image/jpeg;base64,' + imageData
+            });
+            _this.storage.set('photos', _this.photos);
+        }, function (err) {
+            console.log("Camera issue:" + err);
+        });
+    };
+    PhotoService.prototype.loadSaved = function () {
+        var _this = this;
+        this.storage.get('photos').then(function (photos) {
+            _this.photos = photos || [];
+        });
+    };
+    PhotoService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
+            providedIn: 'root'
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_ionic_native_camera_ngx__WEBPACK_IMPORTED_MODULE_2__["Camera"], _ionic_storage__WEBPACK_IMPORTED_MODULE_3__["Storage"]])
+    ], PhotoService);
+    return PhotoService;
+}());
+
+var Photo = /** @class */ (function () {
+    function Photo() {
+    }
+    return Photo;
+}());
+
+
+/***/ }),
+
+/***/ "./src/app/tab2/tab2.module.ts":
+/*!*************************************!*\
+  !*** ./src/app/tab2/tab2.module.ts ***!
+  \*************************************/
+/*! exports provided: Tab2PageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab2PageModule", function() { return Tab2PageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _tab2_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./tab2.page */ "./src/app/tab2/tab2.page.ts");
+
+
+
+
+
+
+
+var Tab2PageModule = /** @class */ (function () {
+    function Tab2PageModule() {
+    }
+    Tab2PageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_3__["NgModule"])({
+            imports: [
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonicModule"],
+                _angular_common__WEBPACK_IMPORTED_MODULE_4__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_5__["FormsModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild([{ path: '', component: _tab2_page__WEBPACK_IMPORTED_MODULE_6__["Tab2Page"] }])
+            ],
+            declarations: [_tab2_page__WEBPACK_IMPORTED_MODULE_6__["Tab2Page"]]
+        })
+    ], Tab2PageModule);
+    return Tab2PageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/tab2/tab2.page.html":
+/*!*************************************!*\
+  !*** ./src/app/tab2/tab2.page.html ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header>\n  <ion-toolbar>\n    <ion-title>\n      Photo Gallery\n    </ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n  <ion-grid>\n    <ion-row>\n      <ion-col size=\"6\" *ngFor='let photo of photoService.photos'>\n        <img [src]=\"photo.data\" />\n      </ion-col>\n    </ion-row>\n  </ion-grid>\n  <ion-fab vertical=\"bottom\" horizontal=\"center\" slot=\"fixed\">\n    <ion-fab-button (click)=\"photoService.takePicture()\">\n      <ion-icon name=\"camera\"></ion-icon>\n    </ion-fab-button>\n  </ion-fab>\n</ion-content>"
+
+/***/ }),
+
+/***/ "./src/app/tab2/tab2.page.scss":
+/*!*************************************!*\
+  !*** ./src/app/tab2/tab2.page.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL3RhYjIvdGFiMi5wYWdlLnNjc3MifQ== */"
+
+/***/ }),
+
+/***/ "./src/app/tab2/tab2.page.ts":
+/*!***********************************!*\
+  !*** ./src/app/tab2/tab2.page.ts ***!
+  \***********************************/
+/*! exports provided: Tab2Page */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab2Page", function() { return Tab2Page; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _services_photo_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../services/photo.service */ "./src/app/services/photo.service.ts");
+
+
+
+var Tab2Page = /** @class */ (function () {
+    function Tab2Page(photoService) {
+        this.photoService = photoService;
+    }
+    Tab2Page.prototype.ngOnInit = function () {
+        this.photoService.loadSaved();
+    };
+    Tab2Page = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-tab2',
+            template: __webpack_require__(/*! ./tab2.page.html */ "./src/app/tab2/tab2.page.html"),
+            styles: [__webpack_require__(/*! ./tab2.page.scss */ "./src/app/tab2/tab2.page.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_services_photo_service__WEBPACK_IMPORTED_MODULE_2__["PhotoService"]])
+    ], Tab2Page);
+    return Tab2Page;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=tab2-tab2-module.js.map
